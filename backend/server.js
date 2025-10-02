50ffed66a9d9fde9b077ff5e86289ddc7242a64d
nxn404-n <nafeurrahmanadil@gmail.com>
2025-10-02 17:39:08 +0600

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to MindKeep backend!');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
