d8e93fcd224bcd693fa0b201ffbdfcac9634a814
nxn404-n <nafeurrahmanadil@gmail.com>
2025-10-14 12:44:01 +0600

    Add update and delete note

--- backend/Controllers/notesController.js
+++ backend/Controllers/notesController.js
@@ -31,3 +31,68 @@ export const createNote = async (req, res) => {
     })
   }
 }
+
+//Update Note
+export const updateNote = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const { title, content } = req.body;
+
+    if (!title && !content) {
+      return res.status(400).json({
+        error: "At least one of title or content must be provided to update."
+      });
+    }
+
+    const updatedFields = {};
+    if (title !== undefined) updatedFields.title = title.trim();
+    if (content !== undefined) updatedFields.content = content.trim();
+
+    const updatedNote = await Note.findByIdAndUpdate(
+      noteId,
+      { $set: updatedFields },
+      { new: true }
+    );
+
+    if (!updatedNote) {
+      return res.status(404).json({ error: "Note not found" });
+    }
+
+    res.status(200).json({
+      message: "Note updated successfully",
+      note: updatedNote
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+}
+
+//Delete note
+export const deleteNote = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+
+    if (!noteId) {
+      return res.status(400).json({
+        error: "Id is missing"
+      });
+    }
+
+    const deletedNote = await Note.findByIdAndDelete(noteId);
+    if (!deletedNote) {
+      return res.status(404).json({
+        error: "Note not found"
+      });
+    }
+
+    res.status(200).json({
+      message: "Note deleted successfully"
+    })
+  } catch (error) {
+     res.status(500).json({
+      error: error.message
+    });
+  }
+}
